229a596e38807e4d828479938b0c720798c3cdbb
Termux-Code <termuxcodepro@gmail.com>
2025-05-24 20:38:21 +0000

    ✅ V04 ter-code app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'app.dart';
 
-void main() {
+Future<void> main() async {
+  // Asegura que Flutter esté inicializado correctamente
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Inicializa AdMob
+  await MobileAds.instance.initialize();
+
+  // Puedes inicializar Firebase aquí en el futuro:
+  // await Firebase.initializeApp();
+
   runApp(const TermuxScriptsApp());
 }
