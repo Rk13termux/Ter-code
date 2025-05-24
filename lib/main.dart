e5128cff39e3d75ae3f781cb3b9ed383039a8e36
Termux-Code <termuxcodepro@gmail.com>
2025-05-24 22:36:06 +0000

    ✅ V05 ter-code app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'screens/welcome_screen.dart';
+import 'app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
